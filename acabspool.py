c00524418e1332524210ba4f9d298897db5c670b
fpletz <fpletz@fnordicwalking.de>
2010-07-04 05:07:06 +0200

    added gigargoyle protocol and actually send animations

--- acabspool.py
+++ acabspool.py
@@ -1,40 +1,83 @@
 import os
 import sys
 import time
+import socket
+import struct
+
+sys.path = ['..'] + sys.path
 os.environ['DJANGO_SETTINGS_MODULE'] = 'acabed.settings'
 
 from acabed.animations import models
 
 log = lambda s: sys.stdout.write(s + '\n')
 
+
+def ntos(n):
+    if n > 0:
+        return chr(n % 256) + ntos(n/256)
+    return ''
+
+
+def send_animation(s, a):
+    duration = -1
+    header = 0x00009000
+    op_duration = 0x0a
+    op_set_screen = 0x11
+    op_flip = 0x12
+
+    for frame in a.get_data():
+        if frame['duration'] != duration:
+            duration = frame['duration']
+            s.send(struct.pack('!III', header | op_duration, 8+4, duration))
+
+        d = struct.pack('!II', header | op_set_screen,
+                        8 + a.height * a.width * a.depth / 8 * a.channels)
+        for r in frame['rows']:
+            for i in xrange(len(r)/2):
+                d += (chr(int(r[i*2:][:2], 16)))
+
+        s.send(d)
+
+        s.send(struct.pack('!II', header | op_flip, 8))
+
 def main():
-    while 1:
-        job = models.SpoolJob.current()
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    s.connect(('localhost', 43948))
 
-        if job == None:
-            log('no job in spool, randomly picking a playlist')
-            playlist = models.Playlist.objects.order_by('?')[0]
-        else:
-            log('working on job: %s' % str(job))
-            playlist = job.playlist
+    try:
+        while 0xacab:
+            job = models.SpoolJob.current()
 
-        log('playlist: %s' % str(playlist))
-        
-        for a in playlist.animations.all():
-            a.playing = True
-            a.save()
+            if job == None:
+                log('no job in spool, randomly picking a playlist')
+                playlist = models.Playlist.objects.order_by('?')[0]
+            else:
+                log('working on job: %s' % str(job))
+                playlist = job.playlist
 
-            log('playing %s' % str(a))
-            # TODO: send
+            log('playlist: %s' % str(playlist))
+            
+            for a in playlist.animations.all():
+                a.playing = True
+                a.save()
 
-            time.sleep(a.max_duration)
+                log('playing %s' % str(a))
 
-            a.playing = False
-            a.save()
+                send_animation(s, a)
 
-        if job != None:
-            log('deleting %s' % str(playlist))
-            job.delete()
+                time.sleep(a.max_duration)
+
+                a.playing = False
+                a.save()
+
+            if job != None:
+                log('deleting %s' % str(playlist))
+                job.delete()
+
+    except KeyboardInterrupt:
+        pass
+
+    s.close()
 
         
 def daemonize():
